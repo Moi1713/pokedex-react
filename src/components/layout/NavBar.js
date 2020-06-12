db92b7128815f52e9174d946d4e8e7c5b94722f4
Moisés Nelson Flores <nelsonflores17@gmail.com>
2020-06-12 03:22:34 -0600

    Consumo de API, Lista de Pokemon

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -6,7 +6,17 @@ export default class NavBar extends Component {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark">
-                    <a className="navbar-brand col-sm-3 col md-2 mr-0 align-items-center text-light">Pokedex</a>
+                    <a className="navbar-brand" href="#">Pokedex</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Inicio <span className="sr-only">(current)</span></a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </div>
         )
