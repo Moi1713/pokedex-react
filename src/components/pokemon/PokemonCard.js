bf983e987807376a28e5598f4e8d3f592cbb0965
Moisés Nelson Flores <nelsonflores17@gmail.com>
2020-06-13 17:58:44 -0600

    Tarjetas y Lista de Pokemon estilizados

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 
 import styled from 'styled-components';
 
+import loading from '../pokemon/loading.gif';
+
 const Sprite = styled.img`
     width: 5em;
     height: 5em;
+`;
+const Card = styled.div`
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, .25, 1);
+    &:hover {
+        box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    }
 `;
 
 export default class PokemonCard extends Component {
@@ -35,9 +44,13 @@ export default class PokemonCard extends Component {
     render() {
 
         return (
-            <div className="col-md-3 col-sm-6 mb-5">
-                <div className="card">
+            <div className="col-lg-2 col-md-3 col-sm-6 mb-5">
+                <Card className="card">
                     <h5 className="card-header bg-danger text-light">{this.state.pokemonIndex}</h5>
+                    {this.state.imageLoading ? (
+                        <img src={loading} style={{ width: '5em', height: '5em' }}
+                            className="card-img-top rounded mx-auto d-block mt-2"/>
+                    ) : null}
                     <Sprite className="card-img-top rounded mx-auto mt-2"
                         onLoad={() => this.setState({ imageLoading: false })}
                         onError={() => this.setState({ toManyRequests: true })}
@@ -63,7 +76,7 @@ export default class PokemonCard extends Component {
                             }
                         </h6>
                     </div>
-                </div>
+                </Card>
             </div>
         )
     }
